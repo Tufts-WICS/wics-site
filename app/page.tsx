d0b3c1c2c1b0c3fe09b7644ea31e1d362cb5846e
caitlynwei <caitlyn.wei@tufts.edu>
2026-02-02 09:38:45 -0500

    layout for header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Layout from './components/Layout';
+
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-white flex bold text-xl justify-center pt-5">
-        Welcome to the WiCS website! This is the home page
-      </h1>
-    </div>
+    <Layout>
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <h1 className="text-white flex bold text-xl justify-center pt-5">
+            Welcome to the WiCS website! This is the home page
+          </h1>
+        </div>
+      </div>
+    </Layout>
+
   );
 }
