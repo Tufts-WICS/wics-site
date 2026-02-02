e52cf48057e934b4552738e6342cdc17788da55d
Michelle F <michelle.fakrogha@yahoo.com>
2026-02-02 13:03:39 -0500

    added padding, colors, width-- working on addoing images

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,10 @@
 
 export default function Header() {
   return (
-    <div>
-		Header!
-	</div>
+		<header className="w-100% bg-[#16181C] pt-2.5 pb-2.5 pr-[70px] pl-[70px]">
+      <div className="space-between">
+        <img src="components/WiCS.png"/> 
+      </div>
+    </header>
   );
 }
