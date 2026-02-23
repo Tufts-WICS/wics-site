1e0a27bddf37cf764db95ab9ee668b9527490bab
Fakrogha <mfakro01@tufts.edu>
2026-02-23 18:54:36 -0500

    Created all header buttons, left to add photos for 3 more buttons and make them hyperlink to other pages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,18 @@
 
 export default function Header() {
   return (
-    <div>
-		Header!
-	</div>
+    <header>
+      <nav className="headerPadding">
+        <button className="brand"><img src="wics.svg"/></button>
+        <div className="rightButtons">
+        <button>About</button> 
+        {/* TODO-- ADD THE SVGS of this and functionalities */}
+        <button>Events</button>
+        <button>Programs</button>
+        <button>E-Board</button>
+        <button><img src="get_connected.svg"/></button>
+        </div>
+      </nav>
+    </header>
   );
 }
