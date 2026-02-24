f5001b0270687029f2be95e72d588555fe02907c
Fakrogha <mfakro01@tufts.edu>
2026-02-23 19:54:08 -0500

    Added all photos/UI changes-- need to make it hyperlink

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
       <nav className="headerPadding">
         <button className="brand"><img src="wics.svg"/></button>
         <div className="rightButtons">
-        <button>About</button> 
-        {/* TODO-- ADD THE SVGS of this and functionalities */}
-        <button>Events</button>
-        <button>Programs</button>
-        <button>E-Board</button>
+        <button><img src="about.svg"/></button> 
+        {/* TODO-- add link functionalities */}
+        <button><img src="events.svg"/></button>
+        <button><img src="programs.svg"/></button>
+        <button><img src="eboard.svg"/></button>
         <button><img src="get_connected.svg"/></button>
         </div>
       </nav>
