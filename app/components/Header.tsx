8325798d044725137ad2687e95fc8a4851a5d33e
caitlynwei <caitlyn.wei@tufts.edu>
2026-03-02 13:02:14 -0500

    added dynamic design for menu navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,17 +2,36 @@
 import Link from "next/link";
 
 export default function Header() {
+  const navItems = [
+    { href: "/", src: "about.svg", alt: "About" },
+    { href: "/events", src: "events.svg", alt: "Events" },
+    { href: "/programs", src: "programs.svg", alt: "Programs" },
+    { href: "/eboard", src: "eboard.svg", alt: "Eboard" },
+    { href: "/get-connected", src: "get_connected.svg", alt: "Get Connected" },
+  ];
+
   return (
     <header>
       <nav className="headerPadding">
-        <Link href = "/"><img src="wics.svg"/></Link>
+        <Link href="/"><img src="wics.svg" alt="WiCS" /></Link>
         <div className="rightLink">
-        <Link href = "/"><img src="about.svg"/></Link> 
-        <Link href = "/events"><img src="events.svg"/></Link>
-        <Link href = "/programs"><img src="programs.svg"/></Link>
-        <Link href = "/eboard"><img src="eboard.svg"/></Link>
-        <Link href = "/get-connected"><img src="get_connected.svg"/></Link>
+          {navItems.map((item) => (
+            <Link key={item.href} href={item.href}>
+              <img src={item.src} alt={item.alt} />
+            </Link>
+          ))}
         </div>
+
+        <details className="mobileMenu">
+          <summary aria-label="Open navigation menu">☰</summary>
+          <div className="mobileMenuPanel">
+            {navItems.map((item) => (
+              <Link key={`mobile-${item.href}`} href={item.href}>
+                <img src={item.src} alt={item.alt} />
+              </Link>
+            ))}
+          </div>
+        </details>
       </nav>
     </header>
   );
