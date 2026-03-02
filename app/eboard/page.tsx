b2c98be912d32f309e6405ffc429d256578148de
caitlynwei <caitlyn.wei@tufts.edu>
2026-03-02 12:32:28 -0500

    added layout to all pages

--- app/eboard/page.tsx
+++ app/eboard/page.tsx
@@ -1,9 +1,11 @@
+import Layout from '../components/Layout';
+
 export default function Eboard() {
   return (
-    <div>
+    <Layout>
       <h1 className="text-white flex bold text-xl justify-center pt-5">
         This is the eboard page
       </h1>
-    </div>
+    </Layout>
   );
 }
--- app/get-connected/page.tsx
+++ app/get-connected/page.tsx
@@ -1,9 +1,12 @@
+import Layout from '../components/Layout';
+
+
 export default function GetConnected() {
   return (
-    <div>
+    <Layout>
       <h1 className="text-white flex bold text-xl justify-center pt-5">
         This is the social media links & connections page
       </h1>
-    </div>
+    </Layout>
   );
 }
