a2b83fa74d3a3cc007f59dc92d8bdf18d4ff4417
Michelle F <michelle.fakrogha@yahoo.com>
2026-04-06 12:06:08 -0400

    removed random hover

--- app/components/Directory_Header.tsx
+++ app/components/Directory_Header.tsx
@@ -60,14 +60,14 @@ export default function DirectoryHeader() {
       `}</style>
       
       <div className="flex gap-2 shrink-0 self-start mt-1">
-        <button className="hover:scale-105 transition-transform focus:outline-none rotate-180">
+        <button className="rotate-180">
           <svg width="32" height="32" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="52.5" y="52.5" width="51" height="51" rx="8.5" transform="rotate(180 52.5 52.5)" fill={currentPageData.hexColor} stroke={currentPageData.hexBorderColor} strokeWidth="3"/>
             <path d="M40.0607 28.0607C40.6464 27.4749 40.6464 26.5251 40.0607 25.9393L30.5147 16.3934C29.9289 15.8076 28.9792 15.8076 28.3934 16.3934C27.8076 16.9792 27.8076 17.9289 28.3934 18.5147L36.8787 27L28.3934 35.4853C27.8076 36.0711 27.8076 37.0208 28.3934 37.6066C28.9792 38.1924 28.9792 38.1924 30.5147 37.6066L40.0607 28.0607ZM15 27L15 28.5L39 28.5L39 27L39 25.5L15 25.5L15 27Z" fill={isBlackPage ? "white" : "black"}/>
           </svg>
         </button>
         
-        <button className="hover:scale-105 transition-transform focus:outline-none">
+        <button className="">
           <svg width="32" height="32" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="52.5" y="52.5" width="51" height="51" rx="8.5" transform="rotate(180 52.5 52.5)" fill={currentPageData.hexColor} stroke={currentPageData.hexBorderColor} strokeWidth="3"/>
             <path d="M40.0607 28.0607C40.6464 27.4749 40.6464 26.5251 40.0607 25.9393L30.5147 16.3934C29.9289 15.8076 28.9792 15.8076 28.3934 16.3934C27.8076 16.9792 27.8076 17.9289 28.3934 18.5147L36.8787 27L28.3934 35.4853C27.8076 36.0711 27.8076 37.0208 28.3934 37.6066C28.9792 38.1924 28.9792 38.1924 30.5147 37.6066L40.0607 28.0607ZM15 27L15 28.5L39 28.5L39 27L39 25.5L15 25.5L15 27Z" fill={isBlackPage ? "white" : "black"}/>
